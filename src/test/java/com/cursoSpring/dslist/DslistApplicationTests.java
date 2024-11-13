43e370d134cb4cfc9f92a7f2b131592c4b1eeb10
Pedro Henrique <pedro.anunziato.simoes@gmail.com>
2024-11-13 14:33:36 -0300

    project created

---
+++ src/test/java/com/cursoSpring/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoSpring.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
